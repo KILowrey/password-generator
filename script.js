f99417b3eab5db058c1a87acc4b954e9fc12fc61
KIL Bismuth <kiralowrey@gmail.com>
2020-07-15 15:39:54 -0700

    outline for interaction

--- script.js
+++ script.js
@@ -12,3 +12,27 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// "Alerts" for all paramaters
+
+alert("Welcome to the Password Generater!");
+
+alert("*insert instructions here*");
+
+// change to a prompts with a validation check later
+alert("How many characters do you want your password to be?");
+
+// change to a prompt or confirm with a validation check later
+alert('Do you need lowercase letters?');
+
+// change to a prompt or confirm with a validation check later
+alert('Do you need uppercase letters?');
+
+// change to a prompt or confirm with a validation chack later
+alert('Do you need numbers?');
+
+// change to a prompt or confirm with a validation check later
+alert('Do you need special characters?');
+
+// end of "alerts" for all paramaters
+
