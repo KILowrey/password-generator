641ca53001053979bc1cc85ce1d71bb68aa4b339
KIL Bismuth <kiralowrey@gmail.com>
2020-07-16 15:13:41 -0700

    improved arrays and added a notes to self section

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ var generateBtn = document.querySelector("#generate");
 
 // ARRAYS
 
-var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; // 26 long. 0-25.
-var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']; // 26 long. 0-25.
-var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']; // 10 long. 0-9
-var special = ['!', '?', '#', '$', '%', '&', '(', ')', '*', '+', '-', '=', ',', '.', '/', ':', ';', '<', '>', '@', '[', ']', '^', '{', '}', '~'] // 26 long. 0-25
+const lowercase = 'abcdefghijklmnopqrstuvwxyz'.split(''); // 26 long. 0-25.
+const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''); // 26 long. 0-25.
+const numbers = '0123456789'.split(''); // 10 long. 0-9.
+const special = '!?#$%&()*-=,./:;<>@[]^{}~'.split(''); // 26 long. 0-25.
 
 // USER INTERACTION
 
@@ -45,3 +45,9 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// NOTES TO SELF TO HELP OUT LATER
+
+// document.write() can write in HTML document
+
