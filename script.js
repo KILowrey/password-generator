9b9e1c66be0fa6410b8585324195d9ac41d71b50
KIL Bismuth <kiralowrey@gmail.com>
2020-07-17 13:09:58 -0700

    changed remaining vars into lets. added (empty) if/else loop to "pswdLength" prompt. improved comments and spacing

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// ARRAYS
-
+// ARRAYS FOR ALL CHARACTER PARAMATERS
 const lowercase = 'abcdefghijklmnopqrstuvwxyz'.split(''); // 26 long. 0-25.
 const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''); // 26 long. 0-25.
 const numbers = '0123456789'.split(''); // 10 long. 0-9.
@@ -13,37 +12,44 @@ const special = '!?#$%&()*-=,./:;<>@[]^{}~'.split(''); // 26 long. 0-25.
 // introduction to the generater
 alert("Welcome to the Password Generater!");
 alert("Please follow the instructions to set the paramaters for the generater");
-alert("If you make a mistake, you can reset the page");
 
 // instructions for picking the length
 alert("FIRST: Please pick a number between 8 and 128");
-
-// add validation check with an alert that loops you back to this question if you didn't follow the instructions
-var pswdLength = prompt("How many characters do you want your password to be?");
+// Picking the length. add validation check with an alert that loops you back to this question if you didn't follow the instructions
+let pswdLength = prompt("How many characters do you want your password to be?") {
+  if(/*more than 7 less than 129*/) {
+    // set pswdLength to that number and move on
+  } else {
+    alert('That is not a valid length. Please pick any integer from 8 to 128.')
+    // redo this promt
+  }
+};
 
 // instructions for character paramaters
 alert('NOW: select OK for Yes and Cancel for No');
-
-var needLower = confirm('Do you need lowercase letters?');
-
-var needUpper = confirm('Do you need uppercase letters?');
-
-var needNumbers = confirm('Do you need numbers?');
-
-var needSpecial = confirm('Do you need special characters?');
+// true/falses for all of the arrays
+let needLower = confirm('Do you need lowercase letters?');
+let needUpper = confirm('Do you need uppercase letters?');
+let needNumbers = confirm('Do you need numbers?');
+let needSpecial = confirm('Do you need special characters?');
 
 // end of "alerts" for all paramaters
 
-// Write password to the #password input
+//Finally, generate the password
+function generatePassword() {
+  /*insert code for generating password here*/
+}
+
+// Write password to the #password input (writes our password on the page)
 function writePassword() {
-  var password = generatePassword(); // puts password we generate into vaiable "password"
+  var password = generatePassword(); // puts password we generate into variable "password"
   var passwordText = document.querySelector("#password"); // puts the element that has the id "#password" in "passwordText"
 
   passwordText.value = password; // sets the value of the element to password
 
 }
 
-// Add event listener to generate button
+// Add event listener to generate button (writes the password when we click the button)
 generateBtn.addEventListener("click", writePassword);
 
 
@@ -54,3 +60,7 @@ generateBtn.addEventListener("click", writePassword);
 /*  if (needUpper === true) {
   *code for drawing from uppercase array*
 } */
+
+// there /is/ a way to scramble a string/array.
+// I think i will generate 2 of each, one of each as needed, and then scramble
+
